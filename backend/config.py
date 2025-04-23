f972c26dd4698af5dcb5072b5377577bc7cc0508
fenieth <alibeuk@gmail.com>
2025-04-23 20:13:32 +0600

    lalalala secret removed

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-AI_TOKEN = "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"
+AI_TOKEN = "secret_token"
 
 
 # это все должно быть ваше, и не делитесь этим ваще нигде
